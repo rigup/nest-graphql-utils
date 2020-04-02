1eb47cb5ed0e13551769030becb3877e68fc9a57
Matt Bise <matthew.bise@rigup.com>
2020-04-02 11:36:42 -0500

    Add comments to provide context around dataloaders

--- src/decorators/loader.decorator.ts
+++ src/decorators/loader.decorator.ts
@@ -1,8 +1,5 @@
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import {
-  createParamDecorator,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { createParamDecorator, InternalServerErrorException } from '@nestjs/common';
 
 import {
   DataLoaderInterceptor,
@@ -14,14 +11,16 @@ interface LoaderClass {
   new (...args: any[]): BatchLoader<any, any>;
 }
 
-export const Loader = createParamDecorator(
-  async (data: LoaderClass, [_, __, ctx]) => {
-    if (ctx[NEST_LOADER_CONTEXT_KEY] === undefined) {
-      throw new InternalServerErrorException(`
+/**
+ * Use the loader accessor to obtain a BatchLoader instance based on type.
+ * For more info on loader accessor, see dataLoader.interceptor.ts
+ */
+export const Loader = createParamDecorator(async (data: LoaderClass, [_, __, ctx]) => {
+  if (ctx[NEST_LOADER_CONTEXT_KEY] === undefined) {
+    throw new InternalServerErrorException(`
             You should provide interceptor ${DataLoaderInterceptor.name} globally with ${APP_INTERCEPTOR}
           `);
-    }
+  }
 
-    return await ctx[NEST_LOADER_CONTEXT_KEY](data.name);
-  },
-);
+  return await ctx[NEST_LOADER_CONTEXT_KEY](data.name);
+});
--- src/interceptors/dataLoader.interceptor.ts
+++ src/interceptors/dataLoader.interceptor.ts
@@ -17,17 +17,20 @@ export const NEST_LOADER_CONTEXT_KEY = 'NEST_LOADER_CONTEXT_KEY';
 export class DataLoaderInterceptor implements NestInterceptor {
   constructor(private readonly moduleRef: ModuleRef) {}
 
-  public intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Observable<any> {
+  /**
+   * Uses a loader accessor to obtain a BatchLoader based on type.
+   *
+   * The loader accessor is a function that takes a loader type (as a string) and returns a
+   * BatchLoader from the context if it exists. If it doesn't exist yet, it resolves the loader
+   * using Nest's provider resolution and stores it in the context, using the loader type as the key.
+   */
+  public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const graphqlExecutionContext = GqlExecutionContext.create(context);
     const ctx: any = graphqlExecutionContext.getContext();
 
+    // If loader accessor does not already exist on context, create it
     if (ctx[NEST_LOADER_CONTEXT_KEY] === undefined) {
-      ctx[NEST_LOADER_CONTEXT_KEY] = async (
-        type: string,
-      ): Promise<BatchLoader<any, any>> => {
+      ctx[NEST_LOADER_CONTEXT_KEY] = async (type: string): Promise<BatchLoader<any, any>> => {
         if (ctx[type] === undefined) {
           try {
             ctx[type] = this.moduleRef.get<BatchLoader<any, any>>(type, {
